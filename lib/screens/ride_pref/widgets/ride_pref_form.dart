872d71a45e4f056ab9260ccc629aee49eafabc9f
lyhov <ornchannlyhov@gmail.com>
2025-02-26 21:16:22 +0700

    BLA-004 – Implement Ride Preferences Form

--- lib/screens/ride_pref/widgets/ride_pref_form.dart
+++ lib/screens/ride_pref/widgets/ride_pref_form.dart
@@ -1,19 +1,12 @@
 import 'package:flutter/material.dart';
- 
 import '../../../model/ride/locations.dart';
 import '../../../model/ride_pref/ride_pref.dart';
- 
-///
-/// A Ride Preference From is a view to select:
-///   - A depcarture location
-///   - An arrival location
-///   - A date
-///   - A number of seats
-///
-/// The form can be created with an existing RidePref (optional).
-///
+import 'package:week_3_blabla_project/screens/app_widget/blabla_button.dart';
+import 'package:week_3_blabla_project/screens/app_widget/location_picker.dart';
+import 'package:week_3_blabla_project/screens/app_widget/date_picker.dart';
+import 'package:week_3_blabla_project/screens/app_widget/seat_number_spinner.dart';
+
 class RidePrefForm extends StatefulWidget {
-  // The form can be created with an optional initial RidePref.
   final RidePref? initRidePref;
 
   const RidePrefForm({super.key, this.initRidePref});
@@ -28,38 +21,95 @@ class _RidePrefFormState extends State<RidePrefForm> {
   Location? arrival;
   late int requestedSeats;
 
-
-
-  // ----------------------------------
-  // Initialize the Form attributes
-  // ----------------------------------
-
   @override
   void initState() {
     super.initState();
-    // TODO 
+    departure = widget.initRidePref?.departure;
+    arrival = widget.initRidePref?.arrival;
+    departureDate = widget.initRidePref?.departureDate ?? DateTime.now();
+    requestedSeats = widget.initRidePref?.requestedSeats ?? 1;
   }
 
-  // ----------------------------------
-  // Handle events
-  // ----------------------------------
- 
+  void _updateDeparture(Location newLocation) {
+    setState(() {
+      departure = newLocation;
+    });
+  }
 
-  // ----------------------------------
-  // Compute the widgets rendering
-  // ----------------------------------
-  
+  void _updateArrival(Location newLocation) {
+    setState(() {
+      arrival = newLocation;
+    });
+  }
 
-  // ----------------------------------
-  // Build the widgets
-  // ----------------------------------
+  void _updateDepartureDate(DateTime newDate) {
+    setState(() {
+      departureDate = newDate;
+    });
+  }
+
+  void _updateSeats(int newSeats) {
+    setState(() {
+      requestedSeats = newSeats;
+    });
+  }
+
+  void _switchLocations() {
+    setState(() {
+      final temp = departure;
+      departure = arrival;
+      arrival = temp;
+    });
+  }
+
+  void _onSearchPressed() {
+    if (departure != null && arrival != null) {
+      RidePref ridePref = RidePref(
+        departure: departure!,
+        arrival: arrival!,
+        departureDate: departureDate,
+        requestedSeats: requestedSeats,
+      );
+
+      Navigator.pop(context, ridePref);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [ 
- 
-        ]);
+      mainAxisAlignment: MainAxisAlignment.start,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        LocationPicker(
+          label: "Departure",
+          initialLocation: departure,
+          onLocationSelected: _updateDeparture,
+        ),
+        IconButton(
+          icon: Icon(Icons.swap_vert),
+          onPressed: _switchLocations,
+        ),
+        LocationPicker(
+          label: "Arrival",
+          initialLocation: arrival,
+          onLocationSelected: _updateArrival,
+        ),
+        DatePicker(
+          initialDate: departureDate,
+          onDateSelected: _updateDepartureDate,
+        ),
+        SeatNumberSpinner(
+          initialValue: requestedSeats,
+          onChanged: _updateSeats,
+        ),
+        BlaButton(
+          text: "Search",
+          onPressed: _onSearchPressed,
+          isPrimary: true,
+          icon: Icons.search,
+        ),
+      ],
+    );
   }
 }
